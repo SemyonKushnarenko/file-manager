7c02145a5fa725ec8afbef898fe322bbce43edde
Trisemya <trisemya@gmail.com>
2022-06-11 02:07:55 +0300

    feat: add some standard requirements issues

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import os from 'os'
+import {stdout, stdin, exit} from 'process'
+import * as readline from 'readline'
 
 import { greeting } from "./src/greeting.js"
 
@@ -7,9 +9,25 @@ const currentDirectory = os.userInfo().homedir
 const currentDirectoryString = `You are currently in ${currentDirectory}\n`
 
 greeting(userName)
-process.stdout.write(currentDirectoryString)
+stdout.write(currentDirectoryString)
 
-process.stdin.on('data', dataBuffer => {
+const rl = readline.createInterface({input: stdin, output: stdout});
 
-  process.stdout.write(currentDirectoryString)
+rl.on('line', dataBuffer => {
+  const data = dataBuffer.toString().trim()
+  try {
+    switch (data) {
+      case '.exit':
+        exit()
+      default:
+        console.log('\x1b[31m%s\x1b[0m', 'Invalid input')
+        break;
+    }
+  } catch(err) {
+    console.log('\x1b[31m%s\x1b[0m', 'Operation failed')
+  }
+
+  stdout.write('\n' + currentDirectoryString)
 })
+
+process.on('exit', () => stdout.write(`\nThank you for using File Manager, ${userName}!`))

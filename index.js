dcc6586bfae2871713244b5325afe29f99e3f091
Trisemya <trisemya@gmail.com>
2022-06-11 13:42:51 +0300

    feat: add os requirements issues

--- index.js
+++ index.js
@@ -25,6 +25,27 @@ rl.on('line', dataBuffer => {
         const pathToHashFile = path.join(currentDirectory, data.split(' ')[1])
         calcHash(pathToHashFile)
         break
+      case data.match(/os\s--.+/)?.input:
+        switch (data.split(' ')[1].slice(2)) {
+          case "cpus":
+            console.log(os.cpus())
+            break
+          case "homedir":
+            console.log(os.userInfo().homedir)
+            break
+          case "username":
+            console.log(os.userInfo().username)
+            break
+          case "EOL":
+            console.log(os.EOL)
+            break
+          case "architecture":
+            console.log(os.arch())
+            break
+          default:
+            console.log("Invalid input")
+        }
+        break
       default:
         console.log('\x1b[31m%s\x1b[0m', 'Invalid input')
         break
